085e7d6e5f1e5c287192da6666ce9320c6d05f67
Justyn Temme <justyntemme@gmail.com>
2023-03-02 10:20:38 -0600

    Try and incrase ability using native python libaries

---
+++ main.py
@@ -0,0 +1,33 @@
+import argparse
+import openai
+
+def is_ai_generated(api_key, paragraph):
+    openai.api_key = api_key
+    prompt = "Is the following paragraph human-written or AI-generated?\n\n" + paragraph + "\n\nAnswer: "
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        temperature=0,
+        max_tokens=40,
+        n=1,
+        stop=None,
+        timeout=10
+    )
+    answer = response.choices[0].text.strip().lower()
+    if answer == "ai-generated":
+        return True
+    elif answer == "human-written":
+        return False
+    else:
+        return None
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--key", type=str, required=True, help="OpenAI API key")
+    parser.add_argument("--text", type=str, required=True, help="Paragraph text to analyze")
+    args = parser.parse_args()
+
+    if is_ai_generated(args.key, args.text):
+        print("The paragraph is AI-generated.")
+    else:
+        print("The paragraph is human-written.")
